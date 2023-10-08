479291bf7b271409e8ed3ece4b51f418f424460b
prav <pravasith@gmail.com>
2023-10-08 17:02:13 -0400

    feat: glfw static import

--- Messages/Recruiter.cpp
+++ Messages/Recruiter.cpp
@@ -1,9 +1,24 @@
 #include "Recruiter.h"
-#include <GLTF/gltf3.h>
+#include "../include/glfw3.h"
+#include <iostream>
 
 Recruiter::Recruiter() {}
 Recruiter::~Recruiter() {}
 
-void Recruiter::SetRecruiter(int data) { __recruiter = data; }
+void Recruiter::SetRecruiter(int data) {
+  /* GLFWwindow *window; */
+
+  int a = glfwInit();
+
+  const char *text = glfwGetClipboardString(NULL);
+
+  if (text) {
+    std::cout << *(text + 1);
+  }
+
+  __recruiter = a;
+}
+
 void Recruiter::GetRecruiter(int &data) { data = __recruiter; }
+
 int Recruiter::GetRecruiter() { return __recruiter; }
