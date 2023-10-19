f340791feaf3b41d17c5c491a5e22dd9452e4000
prav <pravasith@gmail.com>
2023-10-18 22:03:11 -0400

    Glad added

--- main.cpp
+++ main.cpp
@@ -1,6 +1,13 @@
+// clang-format off
+#include <glad/glad.h>
+//
+// clang-format on
 #include "CopyPaste/CopyPaste.h"
 #include "Messages/Engineer.h"
 #include "ResumeContent/CraftResume.h"
+
+#include <GLFW/glfw3.h>
+
 #include <cstdlib>
 #include <iostream>
 #include <string>
@@ -27,6 +34,27 @@ int main() {
   /* Engineer engineer; */
   /* CraftResume craftResume; */
   /* craftResume.Init(); */
+
+  glfwInit();
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+  glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+  // glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+
+  GLFWwindow *window = glfwCreateWindow(800, 600, "LearnOpenGL", NULL, NULL);
+  if (window == NULL) {
+    std::cout << "Failed to create GLFW window" << std::endl;
+    glfwTerminate();
+    return -1;
+  }
+  glfwMakeContextCurrent(window);
+
+  if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+    std::cout << "Failed to initialize GLAD" << std::endl;
+    return -1;
+  }
+
+  glViewport(0, 0, 800, 600);
 
   return EXIT_SUCCESS;
 }
