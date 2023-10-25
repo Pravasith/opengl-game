7ae713b277bcd3aeb3dac9b63e2bd14c4a9fed7e
prav <pravasith@gmail.com>
2023-10-25 13:33:49 -0400

    feat: added quicksort cpp

--- Algorithms/QuickSort.cpp
+++ Algorithms/QuickSort.cpp
@@ -1,5 +1,6 @@
 #include "QuickSort.h"
 #include <iostream>
+#include <ostream>
 
 /*
  * What is QuickSort?
@@ -46,6 +47,8 @@
  *    ^ - P2
  *
  * No it's not! So we swap -23 with our temporary pivot (P2)
+ * and remember to move P2 to the right by one unit.
+ *
  * After swapping the array now looks like this -
  *
  * 0, -23, 100, 28, 300_000, 2, -4, 5858, 0x38
@@ -59,18 +62,96 @@
  * the end of the list
  *
  * 0, -23, 28, 100, 300_000, 2, -4, 5858, 0x38
+ *             ^ - P1                     ^ - Our chosen pivot
+ *             ^ - P2
+ *
+ * 0, -23, 28, 100, 300_000, 2, -4, 5858, 0x38
+ *             ^    ^ - P1                ^ - Our chosen pivot
+ *             ^ - P2
+ *
+ * 0, -23, 28, 100, 300_000, 2, -4, 5858, 0x38
+ *             ^             ^ - P1       ^ - Our chosen pivot
+ *             ^ - P2
+ *
+ * 0, -23, 28, 2, 300_000, 100, -4, 5858, 0x38
+ *                ^        ^ - P1         ^ - Our chosen pivot
+ *                ^ - P2
+ *                Remember that we need to move P2 by ONE unit
+ *                to the right everytime we "swap"
+ *
+ * 0, -23, 28, 2, -4, 100, 300_000, 5858, 0x38
+ *                    ^    ^ - P1         ^ - Our chosen pivot
+ *                    ^ - P2
+ *
+ * 0, -23, 28, 2, -4, 100, 300_000, 5858,   0x38
+ *                    ^             ^ - P1  ^ - Our chosen pivot
+ *                    ^ - P2
+ *
+ * 0, -23, 28, 2, -4, 100, 300_000, 5858,   0x38
+ *                    ^             ^ - P1  ^ - Our chosen pivot
+ *                    ^ - P2
+ *
+ * So now the array is weakly sorted what you need to
+ * do is make your weak_sort algorithm recursive.
+ *
+ * You need to cut the list in half, and then repeat the algorithm
+ * two times. Run it on the left half, and then run it on the right
+ * half.
+ *
+ * And then cut each side of the list in half again. So now you have
+ * 4 pieces. Then run your weak sort algorithm again on each list.
+ *
+ * Repeat the process again until the list is no longer breakable.
+ *
+ *
  * */
+
+int weakSort(int *a, int l, int r) {
+  int tempPivotIndex = l - 1;
+
+  for (int i = l; i < r; i++) {
+    if (a[i] <= a[r]) {
+      tempPivotIndex++;
+
+      int temp = a[i];
+      a[i] = a[tempPivotIndex];
+      a[tempPivotIndex] = temp;
+    }
+  }
+
+  tempPivotIndex++;
+
+  int temp = a[tempPivotIndex];
+  a[tempPivotIndex] = a[r];
+  a[r] = temp;
+
+  return tempPivotIndex;
+}
+
+void print(int *a, int size) {
+  for (int i = 0; i < size; ++i)
+    std::cout << a[i] << " ";
+
+  std::cout << "\n";
+}
+
+void repeat(int a[], int l, int r) {
+  if (l >= r)
+    return;
+
+  int pivotIndex = weakSort(a, l, r);
+
+  repeat(a, l, pivotIndex - 1);
+  repeat(a, pivotIndex + 1, r);
+}
 
 void QuickSort::sort() {
   int input[] = {100, 0, -23, 28, 300000, 2, -4, 5858, 0x38};
-}
+  int size = 9;
 
-int *qs(int arr[], int lo, int hi) {
-  int p = hi;
-  return arr;
-}
+  print(input, size);
+  repeat(input, 0, 8);
+  print(input, size);
 
-void quick_sort(int arr[]) {
-  int lo = -1, hi = 8;
-  qs(arr, lo, hi);
+  std::cout << std::endl;
 }
